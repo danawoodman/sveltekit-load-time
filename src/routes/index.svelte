e005fc7076acdf63d59f93ba47b92b128202782a
Dana Woodman <dana@danawoodman.com>
2021-04-19 20:33:02 -0700

    🚀

---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+	export async function load({ fetch }) {
+		const start = Date.now();
+		const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+		const json = await res.json();
+		const elapsed = Date.now() - start;
+		console.log({ elapsed, start });
+		return { props: { json, start, elapsed } };
+	}
+</script>
+
+<script lang="ts">
+	// export let json;
+	export let elapsed;
+	export let start;
+</script>
+
+<ul>
+	<li>Elapsed: {elapsed}</li>
+	<li>Start: {start}</li>
+</ul>
+
+<!-- <pre>{JSON.stringify(json, null, 2)}</pre> -->
